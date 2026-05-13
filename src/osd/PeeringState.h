ef28fe7bdd22f38be5b66dd62bccb61155a60d1d
Adam Lyon-Jones <adamlyon@uk.ibm.com>
2026-05-13 16:34:51 +0100

    osd: Quiesce in flight pool migrations if there are unfound objects
    * PrimaryLogPG::handle_pool_migration_copy_failure now pauses (or
      "quiesces", if you want to be fancy) to wait for all pool migrations
    in flight to complete before signalling migration_unfound.
    * Added separate logic for handling errors that are not
      unfound (-ENOENT).
    
    Signed-off-by: Adam Lyon-Jones <adamlyon@uk.ibm.com>

--- src/osd/PeeringState.h
+++ src/osd/PeeringState.h
@@ -586,6 +586,12 @@ public:
   TrivialEvent(PoolMigrationStoppedUnfound)
   TrivialEvent(PoolMigrationStoppedTooFull)
   TrivialEvent(PoolMigrationStoppedRevoked)
+
+  struct PoolMigrationStoppedError : boost::statechart::event< PoolMigrationStoppedError > {
+    int error_code;
+    explicit PoolMigrationStoppedError(int err) : error_code(err) {}
+  };
+
   TrivialEvent(StopTargetPoolMigration)
 
   TrivialEvent(AllReplicasActivated)
@@ -1081,7 +1087,8 @@ public:
       boost::statechart::custom_reaction< DeferPoolMigration >,
       boost::statechart::custom_reaction< PoolMigrationStoppedUnfound >,
       boost::statechart::custom_reaction< PoolMigrationStoppedTooFull>,
-      boost::statechart::custom_reaction< PoolMigrationStoppedRevoked >
+      boost::statechart::custom_reaction< PoolMigrationStoppedRevoked >,
+      boost::statechart::custom_reaction< PoolMigrationStoppedError >
       > reactions;
     explicit MigratingSource(my_context ctx);
     void migration_release_reservations();
@@ -1091,6 +1098,7 @@ public:
     boost::statechart::result react(const PoolMigrationStoppedUnfound& evt);
     boost::statechart::result react(const PoolMigrationStoppedTooFull& evt);
     boost::statechart::result react(const PoolMigrationStoppedRevoked& evt);
+    boost::statechart::result react(const PoolMigrationStoppedError& evt);
     void exit();
   };
 
